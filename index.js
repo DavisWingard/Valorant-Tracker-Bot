b82a972e858e854fdaad16dc30f7f4aa7b0c5672
Davis Wingard <davisewingard@gmail.com>
2022-12-16 13:31:31 -0800

    bot reacts to messageCreate event by sending user message.content in a dm

--- index.js
+++ index.js
@@ -1,7 +1,22 @@
 import dotenv from "dotenv";
-import { Client, GatewayIntentBits } from "discord.js";
-
 dotenv.config();
 
-const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages] });
+import { Client, GatewayIntentBits } from "discord.js";
+
+const client = new Client({
+    intents: [
+        GatewayIntentBits.Guilds,
+        GatewayIntentBits.GuildMessages,
+        GatewayIntentBits.MessageContent,
+    ]
+});
 client.login(process.env.TOKEN);
+
+client.on("ready", () => {
+    console.log(`${client.user.tag} has logged in`);
+});
+
+client.on("messageCreate", (message) => {
+    console.log(message.content);
+    message.author.send(`${message.content}`);
+});
