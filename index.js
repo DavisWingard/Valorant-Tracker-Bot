196a30861e78bd86a935a55a82aa4876b47d0e46
Davis Wingard <davisewingard@gmail.com>
2023-04-12 15:08:20 -0700

    puppeteer scrapes rank of player submitted in discord command. agent tracking is no longer a feature of the bot

--- index.js
+++ index.js
@@ -35,12 +35,6 @@ const commands = [
         type: 3,
         required: true,
       },
-      {
-        name: 'agent',
-        description: 'agent',
-        type: 3,
-        required: true,
-      },
       ],
     },
 ];
@@ -73,16 +67,23 @@ client.on('interactionCreate', async interaction => {
     if (!interaction.isChatInputCommand()) return;
   
     if (interaction.commandName === 'help') {
-      await interaction.reply('Enter /track, followed by your Riot ID name, Riot ID tag, and the agent you wish to see stats for');
+      await interaction.reply('Enter /track, followed by your Riot ID name and Riot ID tag. If the bot does not respond, that means that the command was typed improperly, or blitz.gg recognizes the account as a private account.');
     }
     else if (interaction.commandName === 'track') {
       await interaction.reply('track text');
       console.log(interaction.options.getString('riotidname'));
       console.log(interaction.options.getString('riotidtag'));
-      console.log(interaction.options.getString('agent'));
-      scrapeData("https://blitz.gg/valorant/profile/FEVUS-1234");
+
+      const link = "https://blitz.gg/valorant/profile/" + interaction.options.getString('riotidname') + "-" + interaction.options.getString('riotidtag') + "?queue=competitive";
+      scrapeData(link);
     }
 });
+
+function delay(time) {
+  return new Promise(function(resolve) { 
+      setTimeout(resolve, time)
+  });
+}
 
 async function scrapeData(url) {
   const browser = await puppeteer.launch({
@@ -91,10 +92,15 @@ async function scrapeData(url) {
   });
   const page = await browser.newPage();
   await page.goto(url);
+  await delay(6000);
 
-  const [el] = await page.$x('//*[@id="main-content"]/div[1]/div[2]/div/div[2]/section/div[1]/div[1]/div[2]/h4');
-  const txt = await el.getProperty("textContent");
-  const rawTxt = await txt.jsonValue();
+  const overviewElements = await page.$x('//*[@id="main-content"]/div[1]/div[1]/div/div[1]/a[1]')
+  await overviewElements[0].click()
 
-  console.log(rawTxt);
+  const [el] = await page.$x('//*[@id="main-content"]/div[1]/div[1]/div/div[2]/div/div[1]/section[1]/div/div[2]/div[2]/div/div/p');
+  const rankText = await el.getProperty("textContent");
+  const rank = await rankText.jsonValue();
+
+  console.log(rank);
 }
+
